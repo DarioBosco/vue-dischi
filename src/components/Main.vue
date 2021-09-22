6cbd75c38468c4d4eb078d68d60dedb2cb5cce01
DarioBosco <dario_bosco@protonmail.com>
2021-09-22 17:07:52 +0200

    Prima iterazione della API utilizzata

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,31 @@
 <template>
-	<main></main>
+	<main>
+		<div v-for="(album, index) in albums" :key="index">{{ album.title }}</div>
+	</main>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
 	name: 'Main',
+	data() {
+		return {
+			URL: 'https://flynn.boolean.careers/exercises/api/array/music',
+			albums: [],
+		};
+	},
+	methods: {
+		fetchAlbums() {
+			axios.get(this.URL).then((result) => {
+				//console.log(result.data.response);
+				this.albums = result.data.response;
+				console.log(this.albums);
+			});
+		},
+	},
+	mounted() {
+		this.fetchAlbums();
+	},
 };
 </script>
 
