edfa92741e9a18bb719535bef200b1a0bd1aac9b
DarioBosco <dario_bosco@protonmail.com>
2021-09-22 16:43:16 +0200

    Header popolato

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,39 @@
 <template>
-	<header></header>
+	<header>
+		<div class="image-box">
+			<img :src="logo" alt="" />
+		</div>
+	</header>
 </template>
 
 <script>
 export default {
 	name: 'Header',
+	data() {
+		return {
+			logo: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Spotify_icon.png',
+		};
+	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../styles/general';
+@import '../styles/mixins';
+header {
+	height: 70px;
+	width: 100%;
+	background-color: grey;
+	.image-box {
+		height: 100%;
+		width: 10%;
+		@include center(vertical);
+		margin-left: 10px;
+		img {
+			height: 100%;
+			padding: 10px;
+		}
+	}
+}
+</style>
